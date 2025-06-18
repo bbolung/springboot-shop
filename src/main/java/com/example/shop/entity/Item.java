13c3dbc9de5f3379abdb6603635947b87a1bc483
bbolung <shoh2802@gmail.com>
2025-06-18 18:00:03 +0900

    0618

---
+++ src/main/java/com/example/shop/entity/Item.java
@@ -0,0 +1,42 @@
+package com.example.shop.entity;
+
+import com.example.shop.constant.ItemSellStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "item")
+@Getter
+@ToString
+@Setter
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;            //상품코드
+
+    @Column(nullable = false, length = 50)  //Not Null, 최대 50까지 입력 가능
+    private String itemNm;      //상품명
+
+    @Column(nullable = false, name = "price")
+    private int price;          //가격
+
+    @Column(nullable = false)
+    private int stockNumber;    //재고수량
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;  //상품 상세 설명
+
+    @Enumerated(EnumType.STRING)    //항상 STRING 사용
+    private ItemSellStatus itemSellStatus;     //상품 판매 상태
+
+    private LocalDateTime regTime;      //등록 시간
+
+    private LocalDateTime updateTime;   //수정 시간
+
+}
---
+++ src/main/java/com/example/shop/repository/ItemRepository.java
@@ -0,0 +1,27 @@
+package com.example.shop.repository;
+
+import com.example.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+                                                    //클래스이름, 기본키 타입
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);         //itemNm 일치하는 경우만 조회
+
+    List<Item> findByItemNmLike(String itemNm);     //itemNm이 포함되는 경우 모두 조회
+    
+    List<Item> findByPriceLessThan(int price);      //price보다 작은 경우 조회
+
+    //JPQL : entity 객체 이용
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    //nativeQuery : DB table 이용
+    @Query(value = "select * from item where item_detail " +
+            "like %:itemDetail% order by price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+}
