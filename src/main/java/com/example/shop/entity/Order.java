23caf9f9dba1e5c45067d0036670b6cbfacfb112
bbolung <shoh2802@gmail.com>
2025-06-23 17:59:47 +0900

    0623

--- src/main/java/com/example/shop/entity/Order.java
+++ src/main/java/com/example/shop/entity/Order.java
@@ -26,7 +26,7 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> orderItems = new ArrayList<>();  //여러 개 전달받을 수 O
 
     private LocalDateTime orderDate;    //주문일
